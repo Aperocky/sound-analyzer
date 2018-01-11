650415c859be17c2b1306e150cd34cbfa5d542bb
Rocky Li <zl131@duke.edu>
2018-01-11 12:43:36 -0500

    More functions

--- analyze.py
+++ analyze.py
@@ -6,125 +6,71 @@ cdir = os.getcwd()
 sys.path.append(cdir)
 from scipy import signal
 from matplotlib import pyplot as plt
-import matplotlib.animation as animation
 
+def load_npy(filename):
+    return np.load(filename)
 
-def fft(nparray):
-    freq = np.fft.fft(nparray)
-    half = np.arange(1,len(freq)/2+1,dtype=int)
-    freq = freq[100:-100]
-    nfreq = np.zeros(len(freq), dtype=complex)
-    nfreq[100:-100] = freq
-    psd = np.sqrt(np.abs((nfreq[half])**2 + (nfreq[-half])**2))
-    return psd
+def spectrum(audio, frequency = 44100):
+    f, psd = signal.welch(audio, frequency, nperseg = 4096, window = 'hamming')
+    cutoff = 500
+    f = f[:cutoff]
+    psd = psd[:cutoff]
+    # print(psd)
+    return f, psd
 
-def split(nparray, npart = 1):
-    if npart == 1:
-        return [nparray]
-    intervals = np.split(nparray, npart)
-    return intervals
+# Improved version of max_in_range
+def max_range(data, interval):
+    leng = len(data)
+    maxdex = list()
+    middle = int(interval/2)
+    for i in range(leng - interval):
+        # Padding increases the range as frequency increases
+        padding = int(i/50)
+        if i+interval+padding < leng:
+            local = data[i-padding:i+interval+padding]
+        else:
+            break
+        localmax = np.max(local)
+        if discard_max(local, localmax):
+            continue
+        if data[i+middle] == localmax:
+            index = i + middle
+            maxdex.append(index)
+    return maxdex
 
-def spectrum(intervals):
-    psdlist = []
-    for each in intervals:
-        if len(each.shape) > 1:
-            each = each[:, 0]
-        f, psd = signal.welch(each, fs, nperseg = 2048, window = 'hamming')
-        frequency = f
-        psdlist.append(psd)
-    psdlist = np.asarray(psdlist)
-    return frequency, psdlist
+# Helper function for improved max_range
+def discard_max(interval, amax, threshold = 1.5):
+    minimum = np.min(interval)
+    if amax/minimum < threshold:
+        return True
+    return False
 
-def get_max(nparray):
-    last = nparray[0]
-    current = nparray[1]
-    maxlist = []
-    for i in range(len(nparray)-2):
-        if current > nparray[i] and current > last:
-            if current > 1e-12:
-                maxlist.append((i+1, current))
-        last = current
-        current = nparray[i]
-    return maxlist
-
-def init():
-    global line, recordline
-    return line, recordline
-
-def update(i):
-    global line, frequency, psdmatrix, text, recordline, ax
-    text.set_text('Period ' + str(i))
-    line.set_data(frequency, psdmatrix[i])
-    maxlist = get_max(psdmatrix[i])
-    # print(maxlist)
-    # texts = []
-    # for each in maxlist:
-    #     freq = frequency[each[0]]
-    #     texts.append(ax.text(freq, each[1]*3, 'Peak %.02f' % freq))
-    return line, recordline
-
-def checksource(soundarray):
-    plt.plot(soundarray)
-    plt.show()
-    plt.close()
-
-if __name__ == '__main__':
-
-    # INPUT ARGUMENTS:
-    # FILENAME :  file to be opened
-    # fs :  Sampling rate of that file
-    # npart :  Frames numbers for FFT(welch) analysis.
-    if len(sys.argv) < 2:
-        sys.exit('not enough arguments')
-    filename = sys.argv[1]
-    fs = 44100
-    npart = 1
-    background = ''
-    background_select = 3
-    if len(sys.argv) > 2:
-        fs = int(sys.argv[2])
-    if len(sys.argv) > 3:
-        npart = int(sys.argv[3])
-    if len(sys.argv) > 4:
-        background = sys.argv[4]
-    if len(sys.argv) > 5:
-        background_select = int(sys.argv[5])
-
-    # GET SOUND ARRAY
-    soundarray = np.load(filename)
-    if not background == '':
-        base_soundarray = np.load(background)
-        base_sound = spectrum([base_soundarray])[1][0]
-    # checksource(soundarray)
-    # checksource(base_soundarray)
-
-    # SPLIT ARRAY INTO FRAMES:
-    intervals = split(soundarray, npart=npart)
-
-    # Calculate Spectrums in 2d matrix
-    frequency, psdmatrix = spectrum(intervals)
-    # if npart < len(soundarray)/fs:
-    #     psdmatrix *= len(soundarray)/fs/npart
-
-    # Noise information
-    # checksource(base_sound)
-
-    # print(frequency)
-    # print(psdmatrix)
-
+# Initiate pyplot image
+def init_image():
     fig = plt.figure()
     ax = fig.add_subplot(111)
-    ax.set_ylim([1e-13, 1e-3])
-    ax.set_xlim([0,20000])
-    ax.set_xlabel('Frequency: Hz')
-    ax.set_ylabel('Loudness: (dB)')
     ax.grid(True)
-    line, = ax.semilogy([],[])
-    if not background == '':
-        recordline, = ax.semilogy(frequency, base_sound, c='r')
-    else:
-        recordline = None
-    text = ax.text(10000, 1e-5, '')
+    ax.set_xlabel('Frequency: Hz')
+    ax.set_ylabel('Power Density')
+    return ax
 
-    anim = animation.FuncAnimation(fig, update, frames = npart, interval = 500, init_func = init)
+def pin_grapher(ax, frequency, maxdex, audio):
+    print(maxdex)
+    ax.scatter(frequency[maxdex], audio[maxdex], c= 'b')
+    for each in maxdex:
+        ax.text(frequency[each], audio[each], 'Peak at %.0f' % frequency[each])
+
+# Given a graph, plot it onto the ax object
+def semi_graph(ax, x, y, c = 'r', label = ''):
+    ax.semilogy(x,y, color=c, label = label)
+
+if __name__ == '__main__':
+    filename = sys.argv[1]
+    audio = load_npy(filename)
+    f, psd = spectrum(audio)
+    ax = init_image()
+    semi_graph(ax, f, psd, label = filename)
+    maxdex = max_range(psd, 20)
+    # print(f)
+    pin_grapher(ax, f, maxdex, psd)
     plt.show()
--- comparator.py
+++ comparator.py
@@ -30,11 +30,58 @@ def semi_graph(ax, x, y, c = 'r', label = ''):
 def std_graph(ax, x, y, c = 'g', label = ''):
     ax.plot(x,y, color=c, label = label)
 
+def pin_grapher(ax, frequency, maxdex, background, audio):
+    for each in maxdex:
+        vert = frequency[each]
+        ax.plot([vert, vert], [background[each], audio[each]], color='k')
+        ax.text(vert, audio[each], 'Peak at %.0f' % vert)
+
+def max_in_range(data, interval):
+    leng = len(data)
+    maxdex = set()
+    for i in range(leng - interval):
+        local = data[i:i+interval]
+        index = np.argmax(local) + i
+        maxdex.add(index)
+    return maxdex
+
+# Improved version of max_in_range
+def max_range(data, interval):
+    leng = len(data)
+    maxdex = set()
+    middle = int(interval/2)
+    for i in range(leng - interval):
+        # Padding increases the range as frequency increases
+        padding = int(i/50)
+        if i+interval+padding < leng:
+            local = data[i-padding:i+interval+padding]
+        else:
+            break
+        localmax = np.max(local)
+        if discard_max(local, localmax):
+            continue
+        if data[i+middle] == localmax:
+            index = i + middle
+            maxdex.add(index)
+    return maxdex
+
+# Helper function for improved max_range
+def discard_max(interval, amax, threshold = 1.5):
+    minimum = np.min(interval)
+    if amax/minimum < threshold:
+        return True
+    return False
+
+def print_maxdex(fs, power, maxdex):
+    for each in maxdex:
+        print('On bin %d, Maximum Frequency at %.01f Hz, POWER OVER BACKGROUND %.01f times' % (each, fs[each], power[each]))
+
 # Given npy array, convert into frequency domain
 def spectrum(audio, frequency = 44100):
-    f, psd = signal.welch(audio, 44100, nperseg = 1024, window = 'hamming')
-    # f = f[:-80]
-    # psd = psd[:-80]
+    f, psd = signal.welch(audio, frequency, nperseg = 4096, window = 'hamming')
+    cutoff = 1400
+    f = f[10:-cutoff]
+    psd = psd[10:-cutoff]
     # print(psd)
     return f, psd
 
@@ -59,13 +106,16 @@ if __name__ == '__main__':
     # Run spectrum analysis
     freq, curve = spectrum(background, fs)
     _, bluecurve = spectrum(audio, fs)
-    # bluecurve = bluecurve * curve[500]/bluecurve[500]
+    # bluecurve = bluecurve * curve[2]/bluecurve[2]
     snratio = bluecurve/curve
 
     # Graph ax
     semi_graph(ax, freq, curve, label = 'background')
     semi_graph(ax, freq, bluecurve, 'b', 'recording')
     std_graph(ax2, freq, snratio, label = 'S/N ratio')
+    maxdex = max_range(bluecurve, 10)
+    print_maxdex(freq, snratio, maxdex)
+    pin_grapher(ax, freq, maxdex, curve, bluecurve)
     ax.legend(loc=2)
     ax2.legend(loc=0)
 
